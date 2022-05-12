e45734e805cfffff47d38428035d941cf7d04fe0
jewdore <jewdore@gmail.com>
2022-05-12 16:22:34 +0800

    add test

---
+++ gp_test.go
@@ -0,0 +1,24 @@
+package gp
+
+import (
+	"github.com/gin-gonic/gin"
+	"testing"
+)
+
+func Test(t *testing.T)  {
+	r := gin.New()
+	gp := GetGP()
+	gp.Use(r)
+
+	err := gp.AddDefaultMetrics()
+	if err != nil {
+		return
+	}
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, "Hello world!")
+	})
+
+	r.Run(":8989")
+
+}
