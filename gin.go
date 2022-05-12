76e4cf0ef13f1f821af3b7f37e8609976dc624dc
jewdore <jewdore@gmail.com>
2022-05-12 17:19:31 +0800

    default metrics append node tag

--- gin.go
+++ gin.go
@@ -3,6 +3,7 @@ package gp
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"os"
 	"strconv"
 	"time"
 )
@@ -21,18 +22,18 @@ func (gp *GP) HandlerFunc() gin.HandlerFunc {
 		responseSize := float64(c.Writer.Size())
 
 		url := c.Request.URL.Path
-
+		node, _ := os.Hostname()
 		if m, exist := gp.Metric("requests_total"); exist {
-			m.IncBy([]string{}, 1.0)
+			m.IncBy([]string{node}, 1.0)
 		}
 		if m, exist := gp.Metric("requests_total_by_uri"); exist {
-			m.IncBy([]string{url, status}, 1.0)
+			m.IncBy([]string{node, url, status}, 1.0)
 		}
 		if m, exist := gp.Metric("request_duration_seconds"); exist {
-			m.Observe([]string{url}, elapsed)
+			m.Observe([]string{node, url}, elapsed)
 		}
 		if m, exist := gp.Metric("response_size_bytes"); exist {
-			m.Observe([]string{}, responseSize)
+			m.Observe([]string{node}, responseSize)
 		}
 	}
 }
@@ -50,25 +51,25 @@ func (gp *GP) AddDefaultMetrics() error {
 			Type:        Counter,
 			Name:        "requests_total",
 			Description: "server received request num",
-			Labels:      nil,
+			Labels:      []string{"node"},
 		},
 		&Metric{
 			Type:        Counter,
 			Name:        "requests_total_by_uri",
 			Description: "server received request num by uri",
-			Labels:      []string{"uri", "code"},
+			Labels:      []string{"node", "uri", "code"},
 		},
 		&Metric{
 			Type:        Histogram,
 			Name:        "request_duration_seconds",
 			Description: "the time server took to handle the request",
-			Labels:      []string{"uri"},
-			Buckets:     []float64{0.1, 0.3, 1.2, 5, 10},
+			Labels:      []string{"node", "uri"},
 		},
 		&Metric{
 			Type:        Summary,
 			Name:        "response_size_bytes",
 			Description: "response sizes in bytes",
+			Labels:      []string{"node"},
 		},
 	})
 }
